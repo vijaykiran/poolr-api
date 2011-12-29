6aca69df10475d7f21289cb3489ce77b67fb1465
vijaykiran <mail@vijaykiran.com>
2011-12-29 22:53:28 +0100

    Upgrade ring version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject poolr-api "0.0.1-SNAPSHOT"
   :description "Lunatech Pool Ladder API"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring "1.0.0"]
+                 [ring "1.0.1"]
                  [net.cgrand/moustache "1.1.0"]
                  [lobos "1.0.0-SNAPSHOT"]
                  [korma "0.2.1"]
