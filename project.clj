b27055997fd9c120fa78dc620eb07ae186e9e57e
vijaykiran <mail@vijaykiran.com>
2011-12-30 14:52:20 +0100

    Exclusions in project.clj for unwanted dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject poolr-api "0.0.1-SNAPSHOT"
   :description "Lunatech Pool Ladder API"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring "1.0.1"]
+                 [ring "1.0.1" :exclusions [org.clojure/clojure
+                                            clj-stacktrace]]
                  [net.cgrand/moustache "1.1.0"]
                  [lobos "1.0.0-SNAPSHOT"]
                  [korma "0.2.1"]
                  [postgresql "9.1-901.jdbc4"]
-                 [clj-yaml "0.3.1"]]
-  :dev-dependencies [[lein-marginalia "0.7.0-SNAPSHOT"]])
+                 [clj-yaml "0.3.1"]])
